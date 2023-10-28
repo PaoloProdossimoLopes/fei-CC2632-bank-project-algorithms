cb80b742287ffd7d2b2c963196a201e27772ce5f
Felipe <marketing@letscode.com.br>
2023-10-28 18:51:09 -0300

    criado a funcao de deposito no banco

--- main.c
+++ main.c
@@ -257,7 +257,43 @@ void realizarDebito(char cpf[], char senha[], float valor, int *resultado) {
     *resultado = 0; // Indica falha
 }
     // Implemente a função de débito aqui
+    void realizarDeposito(char cpf[], float valor, int *resultado) {
+        FILE *arquivo;
+        Cliente cliente;
 
+        // Abre o arquivo em modo leitura e escrita
+        arquivo = fopen("clientes.dat", "r+b");
+
+        // Verifica se o arquivo foi aberto com sucesso
+        if (arquivo == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            *resultado = 0; // Indica falha
+            return;
+        }
+
+        // Procura o cliente com o CPF fornecido
+        while (fread(&cliente, sizeof(Cliente), 1, arquivo) == 1) {
+            if (strcmp(cliente.cpf, cpf) == 0) {
+                // Atualiza o saldo do cliente com o valor depositado
+                cliente.saldo += valor;
+
+                // Move o cursor para a posição correta no arquivo
+                fseek(arquivo, -sizeof(Cliente), SEEK_CUR);
+                // Escreve as informações atualizadas do cliente no arquivo
+                fwrite(&cliente, sizeof(Cliente), 1, arquivo);
+                // Fecha o arquivo
+                fclose(arquivo);
+                *resultado = 1; // Indica sucesso
+                return;
+            }
+        }
+
+        // Cliente não encontrado
+        printf("CPF incorreto.\n");
+        fclose(arquivo);
+        *resultado = 0; // Indica falha
+// Implemente a função de depósito aqui
+    }
 int main() {
     int escolha;
     char senha[20];
@@ -296,6 +332,18 @@ int main() {
                 }
                 break;
             case OPCAO_DEPOSITO:
+                printf("Digite o CPF: ");
+                scanf("%s", cpf);
+                printf("Digite o valor a ser depositado: ");
+                scanf("%f", &valor);
+
+                realizarDeposito(cpf, valor, &resultado);
+
+                if (resultado) {
+                    printf("Depósito realizado com sucesso!\n");
+                } else {
+                    printf("Erro ao realizar depósito. Verifique as informações fornecidas.\n");
+                }
                 break;
             case OPCAO_EXTRATO:
                 break;
